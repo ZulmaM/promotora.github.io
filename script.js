d757ad4dda054a5592fdcdc7db2e7886f13f9d93
Bici devs <zulmacris1995@gmail.com>
2023-08-27 19:40:26 -0300

    se termina de hacer responsive toda la landing page, usando @media para el header, main, footer, y form y bootstrap para las card

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+//seguimos las instrucciones igual que en html primero el comentario y abajo el ejmplo implementado en codigo
 //primero creamos nuestra hoja js, y la vinculamos con nuestro html mediante el script que va siempre
-//antes de cerrar la etiqueta body, y lo que vamos a hacer como solo tenemos una hoja html, vamos a 
-//hacer que al hacer click en el boton iniciar session se abra el formulario de inicio de sesion para eso
-// declaramos  una variable para hacer aparecer y desaparcer el formulario, aca lo que estamos haciendo es 
-//modificar mi html desde el dom entonces decimos var por variable que el boton cuyo id es ButtonIniciar
-//y  la variable con id loginForm
+//antes de cerrar la etiqueta body, y lo que vamos a hacer como solo tenemos una hoja html, entonces lo que hacemos
+// es  modificar mi html desde el dom usando JS, entonces declaramos primero una variable var y le decimos  que 
+//el boton cuyo id es ButtonIniciar  cuando se haga click sobre el, cambie la visibilidad escondiendo el boton y
+// y mostrando  el formulario cuyo id es loginForm 
 var ButtonIniciar = document.getElementById("ButtonIniciar");
 var loginForm = document.getElementById("loginForm");
 
-
+ //Esto establece un evento que a su vez genera uba funcion de escucha ("click") en el elemento con 
+ //el id "ButtonIniciar" que cuando se hace clic en este botón, se ejecutará la función que se encuentra
+ // dentro de las llaves { ... }.
 ButtonIniciar.addEventListener("click", function() {
+//dentro de la funcion realizamos una comprobacion de que el display del form este en none, es decir que este oculto
     if (loginForm.style.display === "none") {
+
+//si es asi al hacer click en el boton se ejecuta esta funcion mostrando el formulario
         loginForm.style.display = "block";
+
+//caso contrario si ya esta visible el form se ejecuta esta linea de codigo
     } else {
         loginForm.style.display = "none";
     }
